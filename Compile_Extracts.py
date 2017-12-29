cef52a9427f52dc8c86f757c2251077763d3a350
Sean Sargent <seans@auto-graph.com>
2017-12-29 11:01:12 -0800

    Combine all the sharded extract files into one giant extract file

---
+++ Compile_Extracts.py
@@ -0,0 +1,16 @@
+# Loop through extract files and jam them all together
+
+import csv
+import pandas as pd
+import os
+
+extract_files = [fn for fn in os.listdir("/home/ubuntu/") if fn.startswith("AUTOGRAPH_201")]
+
+base_output = extract_files[0]
+
+to_append = extract_files[1:]
+
+with open(base_output, "a") as base_reader:
+    for ef in to_append:
+        for chunk in pd.read_csv(ef, sep=";", quotechar='"', quoting=csv.QUOTE_ALL, chunksize=10000):
+            chunk.to_csv(base_reader, header=False, sep=";", quotechar='"', quoting=csv.QUOTE_ALL)
